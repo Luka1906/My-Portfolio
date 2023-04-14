1eea7ef4ef97dcc35a68543e34fa17c6b69bd081
Luka Matovic <lukamatovic1994@gmail.com>
2023-04-14 10:48:25 -0500

    Adding exit animation and finishing skill page

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -1,46 +1,113 @@
-import { delay } from "framer-motion"
-
-export const animationOne = {
-    in: {
-        opacity:1
-    },
-    out: {
-        opacity:0
-    }
-
-}
+import { easeInOut } from "framer-motion";
 
 export const animationTwo = {
-    in: {
-        opacity:1,
-        y:0,
-        scale:1
-
-    },
-    out: {
-        opacity:0,
-        y: '-100vh',
-        scale:0.3
-    }
-
-}
+  in: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+  },
+  out: {
+    opacity: 0,
+    y: "-100vh",
+    scale: 0.3,
+  },
+};
 export const animationThree = {
-    in: {
-        opacity:1,
-        x: -500
-    },
-    out: {
-        opacity:0,
-        x: 400
-    },
-    end: {
-        x: 0,
-        opacity:1
-    }
+  in: {
+    opacity: 1,
+    x: -500,
+  },
+  out: {
+    opacity: 0,
+    x: 400,
+  },
+  end: {
+    x: 0,
+    opacity: 1,
+  },
+};
 
-}
+export const buttonAnimation = {
+  hidden: {
+    // x: "-100vw",
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+      duration: 1,
+    },
+    // x: 0,
+    // transition: {
+    //   type: "spring",
+    //   stiffness: 120,
+    //   delay: 0.3,
+    // },
+  },
+  buttonHover: {
+    backgroundPosition: "center",
+    scale: 1.1,
+  },
+};
+
+export const navBarAnimation = {
+  hidden: {
+    y: -250,
+  },
+  visible: {
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      delay: 0.5,
+    },
+  },
+};
+export const linksHoverAnimation = {
+  linkHover: {
+    originX: 0,
+    scale: 1.2,
+    transition: {
+      type: "spring",
+      stiffness: 500,
+    },
+  },
+};
 
 export const transition = {
-    duration: 0.5,
+  duration: 0.5,
+};
 
-}
+
+export const socialMediaAnimation = {
+  hover: {
+    scale: 1.3,
+    transition: {
+      repeat: Infinity,
+      repeatType: "reverse",
+      duration: 0.25,
+    },
+  },
+};
+
+export const exitAnimation = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: easeInOut },
+  },
+};
+
+
+  
+  
+
+
+
+
